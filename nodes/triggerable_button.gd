5a4edc49c781b0b2a552979c530d240c72c61948
BasilYes <basilyevs@gmail.com>
2024-08-07 12:26:41 +0300

    use relative path

--- nodes/triggerable_button.gd
+++ nodes/triggerable_button.gd
@@ -7,7 +7,7 @@ func _init() -> void:
 	_triggers = [
 		FastUISignalTrigger.new()
 	]
-	_triggers[0]._lock = preload("res://addons/fast_ui/triggers/ui_trigger_lock.tres")
+	_triggers[0]._lock = preload("../triggers/ui_trigger_lock.tres")
 	_triggers[0]._signal_name = "pressed"
 
 
