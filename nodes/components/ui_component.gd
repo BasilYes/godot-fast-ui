c20d2e064a9d2caf178634d4964800871c8c5cce
BasilYes <basilyevs@gmail.com>
2024-09-28 15:40:19 +0300

    fix @tool errors

--- nodes/components/ui_component.gd
+++ nodes/components/ui_component.gd
@@ -26,6 +26,8 @@ var all_layers: bool = false
 var all_visible: bool = false
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
 	if not instigator:
 		instigator = get_parent()
 	if not instigator.has_signal(signal_name):
